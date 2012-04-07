5b6c9c83087744e8f44d27e3b3162e2de327d469
newguy <gdzack@gmail.com>
2012-04-08 09:27:42 +1000

    First commit.

---
+++ setmaplist/src/main/java/simplepractice/example/setmaplist/App.java
@@ -0,0 +1,13 @@
+package simplepractice.example.setmaplist;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
